baaa8c5ccb5abe861857b61ff9a0eeefa301f5d2
1songjim3@gmail.com <>
2022-08-27 02:09:32 -0400

    implement teleport

--- Assets/Scripts/Components/Interaction/Interactables/Teleportor.cs
+++ Assets/Scripts/Components/Interaction/Interactables/Teleportor.cs
@@ -4,20 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Set the type of interactors to interact with by changing the "interactors" field
-[Interaction(interactable:typeof(Teleportor), interactors:typeof(GeneralInteractor))] 
+[Interaction(interactable: typeof(Teleportor), interactors: typeof(GeneralInteractor))]
 public class Teleportor : InteractableObject
 {
-    // TODO: Add the required fields here
-
+    [SerializeField] private Transform Destination;
+    private readonly Dictionary<InteractionType, string> option = new() {
+        { InteractionType.Primary, "Teleported"}
+    };
     // Teleport the interactor to the destination
     public override string OnInteract(Interactor interactor, InteractionType interactType)
     {
-        // TODO: Implement this
+        Transform interactorTransform = interactor.GetComponent<Transform>();
+        interactorTransform.position = Destination.position;
         return Setting.INTERACTION_OK;
     }
 
     public override Dictionary<InteractionType, string> GetInteractionOptions(Type t)
     {
-        throw new NotImplementedException();
+         return option;
     }
 }
