68431f402b8b47c983e666a0cc267d2da7cdc875
1songjim3@gmail.com <>
2023-08-29 00:02:30 -0400

    add stick ai

---
+++ Assets/GameScripts/AI States/Stick State/ChaseState.cs
@@ -0,0 +1,172 @@
+using System;
+using UnityEngine;
+using LobsterFramework.AbilitySystem;
+using LobsterFramework.EntitySystem;
+using LobsterFramework.AI;
+using GameScripts.Abilities;
+using static UnityEngine.EventSystems.EventTrigger;
+
+namespace GameScripts.AI.StickEnemy
+{
+
+    [CreateAssetMenu(menuName = "StateMachine/States/AI/StickEnemy/ChaseState")]
+    public class ChaseState : State
+    {
+        [SerializeField] private float attackRange;
+        private AITrackData trackData;
+        private AbilityRunner abilityRunner;
+        private AbilityRunner playerAbilityRunner;
+        private bool isNextMoveStart;
+        private Transform transform;
+        private Mana manaComponent;
+        private Entity chaseTarget;
+        private Transform targetTransform;
+        private HeavyWeaponAttack.HeavyWeaponAttackPipe heavyAttackPipe;
+        private float maxChargeTime;
+        private float maxGuardChargeTime;
+        private float cycloneChanceIncrease;
+
+
+
+        public override void InitializeFields(GameObject obj)
+        {
+            abilityRunner = controller.AbilityRunner;
+            playerAbilityRunner = controller.PlayerAbilityRunner;
+            trackData = controller.GetControllerData<AITrackData>();
+            manaComponent = abilityRunner.GetAbilityStat<Mana>();
+            transform = obj.transform;
+            heavyAttackPipe = (HeavyWeaponAttack.HeavyWeaponAttackPipe)abilityRunner.GetAbilityPipe<HeavyWeaponAttack>();
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("Chase");
+            controller.ChaseTarget();
+            chaseTarget = controller.target;
+            targetTransform = controller.target.transform;
+            isNextMoveStart = false;
+            cycloneChanceIncrease = 0;
+            playerAbilityRunner.onAbilityEnqueued += OnPlayerAction;
+
+        }
+
+        public override void OnExit()
+        {
+            playerAbilityRunner.onAbilityEnqueued -= OnPlayerAction;
+        }
+        private void OnPlayerAction(Type abilityType)
+        {
+            isNextMoveStart = true;
+        }
+        public Type MeleeAttack()
+        {
+            Debug.Log("enrter attack");
+            if (!controller.target.gameObject.activeInHierarchy)
+            {
+                return typeof(WanderState);
+            }
+            Debug.Log("1");
+            if (controller.TargetInRange(trackData.chaseDistance.Value))
+            {
+                if (controller.TargetInRange(attackRange))
+                {
+                    /*if (!abilityRunner.IsAbilityRunning<Cyclone>()) //if cyclone is ready
+                    {
+                        float cycloneChance = UnityEngine.Random.Range(0f, 1f);
+
+                        if (cycloneChance > 0.6f - cycloneChanceIncrease) //if cyclone
+                        {
+                            abilityRunner.EnqueueAbility<Cyclone>();
+                        }
+                    }
+                    else
+                    {*/
+                        cycloneChanceIncrease += 0.2f;
+                        float attackType = UnityEngine.Random.Range(0f, 1f);
+                        if (attackType > 0.4)
+                        {
+                            Debug.Log("attack");
+                            abilityRunner.EnqueueAbility<LightWeaponAttack>();
+                        }
+                        else
+                        {
+                            Debug.Log("attack");
+                            abilityRunner.EnqueueAbility<HeavyWeaponAttack>();
+                            float randomChargeTime = UnityEngine.Random.Range(0.5f, 1.5f);
+                            maxChargeTime = Time.time;
+                            maxChargeTime += randomChargeTime * heavyAttackPipe.MaxChargeTime;
+                        }
+                    //}
+
+                }
+                return typeof(ChaseState);
+            }
+            return typeof(ChaseState);
+        }
+
+        public Type GuardCheck()
+        {
+            isNextMoveStart = false;
+            if (playerAbilityRunner.IsAbilityRunning<HeavyWeaponAttack>() || playerAbilityRunner.IsAbilityRunning<LightWeaponAttack>())//if player is attacking playerAbilityRunner.IsAbilityRunning<Cyclone>()
+            {
+                if (!abilityRunner.IsAbilityRunning<HeavyWeaponAttack>() && !abilityRunner.IsAbilityRunning<LightWeaponAttack>())//ai is not attacking playerAbilityRunner.IsAbilityRunning<Cyclone>()
+                {
+                    float GuardChance = UnityEngine.Random.Range(0f, 1f);
+                    if (GuardChance > 0.2)
+                    {
+                        abilityRunner.EnqueueAbility<Guard>();
+                        float randomGuardChargeTime = UnityEngine.Random.Range(0.5f, 1f);
+                        maxGuardChargeTime = Time.time;
+                        maxGuardChargeTime += randomGuardChargeTime;
+                    }
+
+                }
+                else
+                {
+                    float GuardChance = UnityEngine.Random.Range(0f, 1f);
+                    if (GuardChance > 0.4)
+                    {
+                        abilityRunner.EnqueueAbility<Guard>();
+                        abilityRunner.Signal<HeavyWeaponAttack>();
+                        abilityRunner.Signal<LightWeaponAttack>();
+                        float randomGuardChargeTime = UnityEngine.Random.Range(0.5f, 1f);
+                        maxGuardChargeTime = Time.time;
+                        maxGuardChargeTime += randomGuardChargeTime;
+                    }
+                }
+            }
+            return null;
+        }
+        public override Type Tick()
+        {
+            if (!controller.target.gameObject.activeInHierarchy || !controller.TargetInRange(trackData.chaseDistance.Value))
+            {
+                controller.ResetTarget();
+                return typeof(WanderState);
+            }
+            controller.LookTowards();
+            if (abilityRunner.IsAbilityRunning<Guard>() && maxGuardChargeTime < Time.time)
+            {
+                abilityRunner.Signal<Guard>();
+            }
+/*            if(abilityRunner.IsAbilityRunning<Cyclone>())
+            {
+                return typeof(ChaseState);
+            }*/
+            if (controller.TargetInRange(attackRange) && isNextMoveStart==true)
+            {
+                GuardCheck();
+            }
+            if (abilityRunner.IsAbilityRunning<HeavyWeaponAttack>() || abilityRunner.IsAbilityRunning<LightWeaponAttack>()|| abilityRunner.IsAbilityRunning<Guard>()) 
+            {
+                if(abilityRunner.IsAbilityRunning<HeavyWeaponAttack>() && maxChargeTime<Time.time )
+                {
+                    abilityRunner.Signal<HeavyWeaponAttack>();
+                }
+                return typeof(ChaseState);
+            }
+            MeleeAttack();
+            return typeof(ChaseState);
+        }
+    }
+}   
---
+++ Assets/GameScripts/AI States/Stick State/WonderState.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LobsterFramework.AI;
+using System.Security.Cryptography;
+using LobsterFramework;
+
+namespace GameScripts.AI.StickEnemy
+{
+    [CreateAssetMenu(menuName = "StateMachine/States/AI/StickEnemy/StickWanderState")]
+    public class WanderState : State
+    {
+        [SerializeField] private RefFloat wanderTime;
+        [SerializeField] private RefInt wanderRadius;
+        [SerializeField] private RefFloat idleTime;
+        private StealthController trans;
+        private float wanderTimeCounter;
+        private float idleTimeCounter;
+        private Rigidbody2D daggerRigid;    
+        private MovementController moveControl;
+        private Transform transform;
+        private AITrackData trackingData;
+        private WanderInternalState wanderState;
+        
+
+        private enum WanderInternalState
+        {
+            Idle,
+            PathFinding,
+            Wandering
+        }
+
+
+        public override void InitializeFields(GameObject obj)
+        {
+            transform = controller.transform;
+            trans = controller.GetComponent<StealthController>();
+            moveControl = controller.GetComponent<MovementController>();
+            daggerRigid = controller.GetComponent<Rigidbody2D>();
+            trackingData = controller.GetControllerData<AITrackData>();
+            
+           
+        }
+
+        public override void OnEnter()
+        {
+            wanderState = WanderInternalState.Idle;
+            wanderTimeCounter = 0;
+            idleTimeCounter = 0;
+        }
+
+        public override void OnExit()
+        {
+
+        }
+
+        public override Type Tick()
+        {
+            float sight = trackingData.sightRange.Value;
+            Debug.DrawLine(transform.position, transform.position + transform.up * sight, Color.yellow);
+            if (controller.SearchTarget(sight))
+            {
+                /*float enemyHealth = controller.target.Health / controller.target.MaxHealth;
+                if (enemyHealth>0.4)
+                {
+                    Debug.Log("fuck u");
+                    return typeof(MeleeChaseState);
+                    //return typeof(RangedChaseState);
+                }
+                else
+                {
+                    float meleeDesire = 0.3f;
+                    float diffinHealth = 0.4f - enemyHealth;
+                    meleeDesire += diffinHealth / 0.4f * 0.7f;
+                    float randomNumber = UnityEngine.Random.Range(0f, 1f);
+                    if(randomNumber > meleeDesire)
+                    {
+                        Debug.Log("fuck u");
+                        //return typeof(RangedChaseState);
+                    }
+                    return typeof(MeleeChaseState);
+                }*/
+                return typeof(ChaseState);
+
+            }
+            switch (wanderState)
+            {
+                case WanderInternalState.Idle:
+                    idleTimeCounter += Time.deltaTime;
+                    if (idleTimeCounter >= idleTime.Value)
+                    {
+                        idleTimeCounter = 0;
+                        wanderState = WanderInternalState.PathFinding;
+                    }
+                    break;
+                case WanderInternalState.PathFinding:
+                    controller.Wander(wanderRadius.Value);
+                    wanderState = WanderInternalState.Wandering;
+                    break;
+                case WanderInternalState.Wandering:
+                    wanderTimeCounter += Time.deltaTime;
+                    if(daggerRigid.velocity!=Vector2.zero)
+                    {
+                        moveControl.RotateTowards(daggerRigid.velocity);
+                    }
+
+                    if (controller.ReachedDestination() || wanderTimeCounter >= wanderTime.Value)
+                    {
+                        wanderTimeCounter = 0;
+                        wanderState = WanderInternalState.Idle;
+                    }
+                    break;
+            }
+            return null;
+        }
+    }
+}
